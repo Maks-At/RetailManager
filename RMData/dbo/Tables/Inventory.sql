df596974b739491c9f3c9d964fa79ea4e0bee956
Maks_At <maks.atagoziyev@gmail.com>
2022-05-22 13:56:41 +0300

    Add Tables
    Added Tables

---
+++ RMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[ProductId] INT NOT NULL,
+	[Quantity] INT NOT NULL DEFAULT 1,
+	[PurchasePrice] MONEY NOT NULL,
+	[PurchaseDate] DATETIME2(2) DEFAULT GETUTCDATE()
+
+
+)
