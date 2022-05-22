4a47b686598a1e88d573d91b263f5e51dcbfd692
Maks_At <maks.atagoziyev@gmail.com>
2022-05-22 22:55:38 +0300

    Add LoginViewModel
    Added LoginViewModel

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Caliburn.Micro;
 
 namespace RMDesktopUI.ViewModels
 {
-    public class ShellViewModel
+    public class ShellViewModel : Conductor<object>
     {
+        public ShellViewModel()
+        {
+            Activate();
+        }
+
+        private async void Activate()
+        {
+            await ActivateItemAsync(IoC.Get<LoginViewModel>(), new CancellationToken());
+            Console.WriteLine("Login view activated");
+        }
     }
 }
