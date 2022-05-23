9022c9161af82f0283cde7612877dfaa8ca21ae0
Maks_At <maks.atagoziyev@gmail.com>
2022-05-24 00:37:34 +0300

    Add Sales Page
    Add the Sales Page and the supporting View Model without the actual logic

---
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -0,0 +1,125 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.ViewModels
+{
+    public class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set
+            {
+                _products = value;
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+
+        private string _itemQuantity;
+
+        public string ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set
+            {
+                _itemQuantity = value;
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public bool CanAddToCart
+        {
+            get
+            {
+                var output = false;
+
+                //somthing should be selected in products
+                //item quantity should be > 0
+
+                return output;
+            }
+        }
+
+        public string SubTotal
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+
+        public string Tax
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                return "$0.00";
+            }
+        }
+
+        public void AddToCart()
+        {
+
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                var output = false;
+
+                //somthing should be selected in the Cart
+                //item quantity should be > 0
+
+                return output;
+            }
+        }
+
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckout
+        {
+            get
+            {
+                var output = false;
+
+                //somthing should be selected in the Cart
+
+                return output;
+            }
+        }
+
+        public void Checkout()
+        {
+
+        }
+    }
+}
